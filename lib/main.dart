7b3feacc8a52727e8476e1b9835b5c41308e3cf9
Aaron Kurian <aaronkurianabraham@gmail.com>
2025-08-30 13:43:42 +0530

    fix: Created a simple layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hello world'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('My First App'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('Hello World'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+      onPressed: () {
+        print('Floating Action Button Pressed');
+      },
+    ),
+  ),
 ));
 
 
