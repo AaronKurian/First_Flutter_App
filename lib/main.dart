f266a7b0df6a4838f8e849c2293b04aacdabafaf
Aaron Kurian <aaronkurianabraham@gmail.com>
2025-08-30 16:25:06 +0530

    fix: Used Images

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,17 @@ class Home extends StatelessWidget {
       backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text(
-        'Hello World',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'Mechanismo',
-          ),
-        ),
+      child: Image.asset('assets/space-3.jpg'),
+      // child: Image.network('https://images.pexels.com/photos/2387877/pexels-photo-2387877.jpeg'),
+
+        // child: Image(
+        //   // image: NetworkImage('https://images.pexels.com/photos/2387877/pexels-photo-2387877.jpeg'),
+        
+        //   image: AssetImage('assets/space-2.jpg'),
+        // ),
       ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
       child: Text('click me'),
       backgroundColor: Colors.red[600],
     ),
