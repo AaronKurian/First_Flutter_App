fa90210c79e09e51191441ac3062dee3c05a7f6e
Aaron Kurian <aaronkurianabraham@gmail.com>
2025-08-30 16:13:46 +0530

    fix: Created a simple stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My First App'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Hello World'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-      onPressed: () {
-        print('Floating Action Button Pressed');
-      },
-    ),
-  ),
+  home: Home(),
 ));
 
 
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('My First App'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+        'Hello World',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'Mechanismo',
+          ),
+        ),
+      ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('click me'),
+      backgroundColor: Colors.red[600],
+    ),
+  );
+  }
+}
